8a7ea26ea70ad52dc15568622c5f1945699bc119
loi.chau <loichau997@gmail.com>
2023-09-09 17:03:37 +0700

    Add pagination functions

---
+++ django/utils/pagination.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.core.paginator import Paginator
+
+def GetPageAndPageSizeFromReuest(request):
+    page = request.GET.get('page', 1)
+    try: 
+        page =  int(page)
+    except ValueError:
+        page = 1
+
+    page_size = request.GET.get('page_size', 5)
+    try: 
+        page_size = int(page_size)
+    except ValueError:
+        page_size = 5
+        
+    return page, page_size
